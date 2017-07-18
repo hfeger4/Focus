cb2935fe8270d8c574c0d4e14598c71cf97206b3
Howard Feger <hfeger4@gmail.com>
2017-07-18 15:00:31 -0700

    add store and update entry

--- flickr_clone/frontend/entry.jsx
+++ flickr_clone/frontend/entry.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import {login, signup, logout} from './util/session_api_util.js';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Focus</h1>, root);
 
   window.login = login;
   window.logout = logout;
   window.signup = signup;
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
