14479ca7a2bdfbffc60113f8e3edd37906b85690
Howard Feger <hfeger4@gmail.com>
2017-07-18 16:27:22 -0700

    add login/logout functionality

--- flickr_clone/frontend/entry.jsx
+++ flickr_clone/frontend/entry.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {login, signup, logout} from './util/session_api_util.js';
 import configureStore from './store/store';
 import Root from './components/root';
+import { login, signup, logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
@@ -11,8 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<Root store={ store }/>, root);
 
   window.login = login;
-  window.logout = logout;
-  window.signup = signup;
 
   window.getState = store.getState;
   window.dispatch = store.dispatch;
