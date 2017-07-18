50fc7916e7d71a51f1c1723201eccda078dd2fb2
Howard Feger <hfeger4@gmail.com>
2017-07-18 15:13:40 -0700

    add root and update entry to accept root

---
+++ flickr_clone/frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>FocusTHIS</h1>
+  </div>
+);
+
+export default App;
--- flickr_clone/frontend/entry.jsx
+++ flickr_clone/frontend/entry.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 
 import {login, signup, logout} from './util/session_api_util.js';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Focus</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 
   window.login = login;
   window.logout = logout;
