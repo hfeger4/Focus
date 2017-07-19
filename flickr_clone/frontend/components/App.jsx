e7529f3d0e83a66fcf6843891c4a0065f5e888cf
Howard Feger <hfeger4@gmail.com>
2017-07-19 07:36:42 -0700

    add reset_css

--- flickr_clone/frontend/components/App.jsx
+++ flickr_clone/frontend/components/App.jsx
@@ -12,11 +12,10 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session_form/session_form_container';
 
-
 const App = () => (
   <div>
     <header>
-      <h1>Welcome to Focus</h1>
+      <Link to='/' className='logo-text'><h1>Welcome to Focus</h1></Link>
       <GreetingContainer />
     </header>
 
