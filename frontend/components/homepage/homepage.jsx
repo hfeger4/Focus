cdaf224e925ed0c8b57c84aeb0a3e9485991bf49
Howard Feger <hfeger4@gmail.com>
2017-07-21 11:20:57 -0700

    index page css

---
+++ frontend/components/homepage/homepage.jsx
@@ -0,0 +1,17 @@
+// <div className="footer">
+//   <div className="footer-icons">
+//     <Link to='/'><i className="fa fa-camera-retro fa-2x"></i></Link>
+//   </div>
+//   <div className="footer-icons">
+//     <i className="fa fa-home fa-2x" aria-hidden="true"></i>
+//
+//   </div>
+//   <div className="footer-icons">
+//     <a href="https://github.com/hfeger4">
+//     <i className="fa fa-github fa-2x" aria-hidden="true"></i>
+//     </a>
+//   </div>
+//   <div className="footer-icons">
+//     <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+//   </div>
+// </div>
--- frontend/components/photos/photos_index.jsx
+++ frontend/components/photos/photos_index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import Modal from 'react-modal';
+import Masonry from 'react-masonry-component';
 
 class PhotosIndex extends React.Component{
   constructor(props){
@@ -11,14 +12,54 @@ class PhotosIndex extends React.Component{
     this.props.fetchPhotos();
   }
 
+
+
+// const { photos } = this.props;
+// const photoItems = photos.map(photo => (
+//   <div key={photo.id}>
+//   <img src={ photo.image_url }/>
+//   </div>
+
+
+// render(){
+//   const { photos } = this.props;
+//   return(
+//     <div className="photo-index">
+//       <div className="photo-items">
+//         {photos.map( photo => (
+//           <div key={photo.id} className="single-photo">
+//             <img src={ photo.image_url }/>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+//   }
+// }
+
   render(){
-    const { photos } = this.props;
-    const photoItems = photos.map(photo => (
-      <img src={photo.image_url}/>
-    ));
-    return(
-      <div>
-        {photoItems}
+    const {photos} = this.props;
+    // const photoItems = photos.map(photo => (
+    //   <div key={photo.id}>
+    //     <img src={ photo.image_url }/>
+    //   </div>
+    // ));
+
+    return (
+      <div className="masonry">
+      <Masonry
+        className={'my-gallery-class'}
+        elementType={'ul'}
+        options={{transitionDuration: 0}}
+        disableImagesLoaded={false}
+        updateOnEachImageLoad={false}
+        >
+        {photos.map( photo => (
+          <div key={photo.id} className="single-photo">
+            <img src={ photo.image_url }/>
+          </div>
+        ))}
+      </Masonry>
       </div>
     );
   }
