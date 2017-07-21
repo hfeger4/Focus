c70811fc4c636018f02f0f31bd47e7d8a83e3c93
Howard Feger <hfeger4@gmail.com>
2017-07-21 09:19:19 -0700

    change tab icon and add seeds before setup

--- frontend/components/photos/photos_index_container.js
+++ frontend/components/photos/photos_index_container.js
@@ -4,7 +4,6 @@ import { fetchPhotos } from '../../actions/photo_actions';
 import PhotosIndex from './photos_index';
 
 const mapStateToProps =  (state) => {
-  console.log(state);
   return {photos: selectAllPhotos(state)};
 
 };
