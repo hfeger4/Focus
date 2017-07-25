4f405dc2ecb852802ca40ae333a9cc677b535f0d
Howard Feger <hfeger4@gmail.com>
2017-07-24 18:09:05 -0700

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,30 +15,50 @@ guest2 = User.create(username: "User", password: "password")
 
 
 #Photos
-photo = Photo.create(title: "camera",
-                     body: "lol",
+photo = Photo.create(title: "Carpe Diem",
+                     body: "Seize the day. For pictures.",
                      image_url: "http://res.cloudinary.com/ds7hk07wv/image/upload/v1500943263/pexels-photo-507561_1_ekh1sk.jpg",
                      user_id: 1)
 photo1 = Photo.create(image_url: "http://res.cloudinary.com/ds7hk07wv/image/upload/v1500943156/skydiving-jump-falling-parachuting-37656_aey05z.jpg",
-                     user_id: 1)
+                     user_id: 1,
+                     title: "Risk Taker",
+                     body: "Just my average saturday.")
 photo2 = Photo.create(image_url: "http://res.cloudinary.com/ds7hk07wv/image/upload/v1500943156/rain-day-car-lights-78497_d1ym6r.jpg",
-                     user_id: 1)
+                     user_id: 1,
+                     title: "Car Wash",
+                     body: "Rainy day or car wash? You decide.")
 photo3 = Photo.create(image_url: "http://res.cloudinary.com/ds7hk07wv/image/upload/v1500943155/pexels-photo-220917_1_m1h3xs.jpg",
-                      user_id: 1)
+                      user_id: 1,
+                      title: "Risk Taker",
+                      body: "Just my average saturday.")
 photo4 = Photo.create(image_url: "http://res.cloudinary.com/ds7hk07wv/image/upload/v1500943156/peacock-bird-plumage-display-50557_1_kyxlwg.jpg",
-                      user_id: 1)
+                      user_id: 1,
+                      title: "Pretty Chicken",
+                      body: "Through careful evolution this chicken evolved to be too beautiful to eat.")
 photo5 = Photo.create(image_url: "http://res.cloudinary.com/ds7hk07wv/image/upload/v1500943155/pexels-photo-301292_1_odpybw.jpg",
-                      user_id: 1)
+                      user_id: 1,
+                      title: "Smoke Break",
+                      body: "Settled on Paris.")
 photo6 = Photo.create(image_url: "http://res.cloudinary.com/ds7hk07wv/image/upload/v1500943155/pexels-photo-261877_1_zasbgg.jpg",
-                      user_id: 1)
+                      user_id: 1,
+                      title: "Reflection",
+                      body: "Pretty picture.")
 photo7 = Photo.create(image_url: "http://res.cloudinary.com/ds7hk07wv/image/upload/v1500943155/pexels-photo-265683_1_vzv2hi.jpg",
-                      user_id: 1)
+                      user_id: 1,
+                      title: "Tech Setup",
+                      body: "2017 tech setup.")
 photo8 = Photo.create(image_url: "http://res.cloudinary.com/ds7hk07wv/image/upload/v1500943156/pexels-photo-518557_kcksal.jpg",
-                      user_id: 1)
+                      user_id: 1,
+                      title: "Bale of Hay",
+                      body: "Another day on the farm.")
 photo9 = Photo.create(image_url: "http://res.cloudinary.com/ds7hk07wv/image/upload/v1500943155/night-product-watch-dramatic-84475_1_own3hm.jpg",
-                      user_id: 2)
+                      user_id: 2,
+                      title: "Classic Watch",
+                      body: "On the to-buy list.")
 photo10 = Photo.create(image_url: "http://res.cloudinary.com/ds7hk07wv/image/upload/v1500942189/beer_gdjakq.jpg",
-                      user_id: 2)
+                      user_id: 2,
+                      title: "Beers and Candles",
+                      body: "There are beers and candles here, very interesting.")
 
 
 #Albums
